8bad58f5e1da4aa04afd64fe80380c84f42a036b
ypc2e55orj <ypc2e55orj@gmail.com>
2025-02-21 11:46:49 +0900

    通常のログも出力されるように

--- dump.py
+++ dump.py
@@ -1,45 +1,43 @@
 import serial
 import datetime
-import logging
 from pathlib import Path
 
 COM = '/dev/ttyACM0'
 RECVSIZE = 1024
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('rt-linelight')
-logger.setLevel(logging.INFO)
 ser = serial.Serial(COM, 912600, timeout=1)
 
-
 def dump(savedir):
-    f = None
-    data = b''
-    recieved = 0
-    logger.info('Waiting for STX...')
+    file = None
+    filename = ''
+    buffer = b''
+    total = 0
     while True:
-        data = ser.read(RECVSIZE)
-        stx = data.find(b'\x02')
-        if stx != -1:
-            now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            logger.info(f'STX is received, create "{now}.csv"')
-            f = open(savedir / f'{now}.csv', 'w')
-            f.write(data[stx + 1:].decode('utf-8'))
-            recieved += len(data[stx + 1:])
+        buffer = ser.read(RECVSIZE)
+        stx = buffer.find(b'\x02')
+        if stx == -1:
+            print(buffer.decode('utf-8'), end='')
+        else:
+            print(buffer[:stx].decode('utf-8'))
+            filename = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.csv'
+            print(f'\x1b[32mSTX is received, create "{filename}"\n')
+            file = open(savedir / filename, 'w')
+            file.write(buffer[stx + 1:].decode('utf-8'))
+            total += len(buffer[stx + 1:])
             break
     while True:
-        data = ser.read(RECVSIZE)
-        etx = data.find(b'\x03')
+        buffer = ser.read(RECVSIZE)
+        etx = buffer.find(b'\x03')
         if etx != -1:
-            recieved += len(data[:etx])
-            f.write(data[:etx].decode('utf-8'))
-            f.close()
-            logger.info(f'ETX is received (Total: {recieved} bytes), close "{now}.csv"')
+            total += len(buffer[:etx])
+            file.write(buffer[:etx].decode('utf-8'))
+            file.close()
+            print(f'ETX is received (Total: {total} bytes), close "{filename}"\x1b[m\n')
             break
         else:
-            f.write(data.decode('utf-8'))
-            recieved += len(data)
-            print(f'\x1b[1A\x1b[2K{recieved:>10} bytes received')
+            file.write(buffer.decode('utf-8'))
+            total += len(buffer)
+            print(f'\x1b[1A\x1b[2K{total:>10} bytes received')
 
 
 def main():
@@ -52,6 +50,7 @@ def main():
     except KeyboardInterrupt:
         pass
     finally:
+        print('\x1b[m')
         ser.close()
 
 
